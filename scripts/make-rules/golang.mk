ebd69bc0cdf29745db87397d2306be858d4e3068
Chever John <cheverjonathan@gmail.com>
2023-07-21 13:48:26 +0800

    chore(makefile): golang

--- scripts/make-rules/golang.mk
+++ scripts/make-rules/golang.mk
@@ -37,7 +37,7 @@ ifeq (${BINS},)
   $(error Could not determine BINS, set ROOT_DIR or run in source dir)
 endif
 
-EXCLUDE_TESTS=github.com/marmotedu/iam/test github.com/marmotedu/iam/pkg/log github.com/marmotedu/iam/third_party github.com/marmotedu/iam/internal/pump/storage github.com/marmotedu/iam/internal/pump github.com/marmotedu/iam/internal/pkg/logger
+EXCLUDE_TESTS=github.com/Chever-John/cas/test github.com/Chever-John/cas/pkg/log github.com/marmotedu/iam/third_party github.com/marmotedu/iam/internal/pump/storage github.com/marmotedu/iam/internal/pump github.com/marmotedu/iam/internal/pkg/logger
 
 .PHONY: go.build.verify
 go.build.verify:
